948eb4eb7383e8ab1186d76a562cbeba45bd395f
Brian Seymour <imbrianseymour@gmail.com>
2019-09-26 11:19:27 -0500

    authorization working

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-extern crate websocket;
-extern crate tokio;
+extern crate json;
+extern crate ws;
 
 mod rapidsockets {
 
-    use std::thread;
-    use websocket::ClientBuilder;
-    use websocket::Message as WsMessage;
-    use websocket::OwnedMessage;
-    use websocket::OwnedMessage::Text;
-    use websocket::client::sync::Client as WsClient;
-    use websocket::stream::sync::NetworkStream;
-    use websocket::stream::sync::TlsStream;
-    use websocket::stream::sync::TcpStream;
+    use json;
+    use ws::{connect, CloseCode};
 
     fn noop(_: String) {}
 
     pub struct Config {
         gateway: String,
         api: String,
-        key: String
+        key: String,
     }
 
     impl Config {
@@ -51,7 +44,7 @@ mod rapidsockets {
         session: String,
         packet_queue: Vec<String>,
         cbs: Vec<fn()>,
-        subscriptions: Vec<Subscription<'a>>
+        subscriptions: Vec<Subscription<'a>>,
     }
 
     impl<'a> Client<'a> {
@@ -67,45 +60,36 @@ mod rapidsockets {
             }
         }
 
-        pub fn connect(&self) {
-            let mut conn = ClientBuilder::new(&self.config.gateway)
-                .unwrap()
-                .connect(None)
-                .unwrap();
-
-            let recv = thread::spawn(move || {
-                loop {
-                    println!("@@@@@@@@@@@@@@");
-                    match conn.recv_message() {
-                        Ok(m) => {
-                            match m {
-                                Text(message) => {
-                                    println!("{:?}", message);
-                                },
-                                _ => {
-                                    println!("{:?}", m);
-                                }
-                            }
-                        },
-                        _ => {}
+        pub fn connect(&mut self) {
+            let packet =
+                json::object!{
+                    "action" => "authorize",
+                    "payload" => json::object!{
+                        "key" => self.config.key.to_string()
                     }
-                }
-            });
+                };
 
-            let _ = recv.join();
+            connect(self.config.gateway.to_string(), |out| {
+                out.send(packet.dump()).unwrap();
+
+                move |msg| {
+                    println!("Got message: {}", msg);
+                    Ok(())
+                }
+            }).unwrap();
         }
 
     }
 
-    pub struct Subscription<'b> {
-        client: &'b Client<'b>,
+    pub struct Subscription<'a> {
+        client: &'a Client<'a>,
         channel: String,
-        callback: fn(String)
+        callback: fn(String),
     }
 
-    impl<'b> Subscription<'b> {
+    impl<'a> Subscription<'a> {
 
-        pub fn init(client: &'b Client<'b>) -> Subscription<'b> {
+        pub fn init(client: &'a Client<'a>) -> Subscription<'a> {
             Subscription {
                 client: client,
                 channel: "".to_string(),
@@ -127,15 +111,15 @@ mod rapidsockets {
 
     }
 
-    pub struct Message<'c> {
-        pub client: &'c Client<'c>,
+    pub struct Message<'a> {
+        pub client: &'a Client<'a>,
         pub channel: String,
         pub message: String,
     }
 
-    impl<'c> Message<'c> {
+    impl<'a> Message<'a> {
 
-        pub fn init(client: &'c Client<'c>) -> Message<'c> {
+        pub fn init(client: &'a Client<'a>) -> Message<'a> {
             Message {
                 client: client,
                 channel: "".to_string(),
@@ -165,8 +149,7 @@ fn main() {
     config.set_api("http://127.0.0.1:2008");
     config.set_key("mul-f75b9b5c-7b50-47ac-b937-c1909242d0ce");
 
-    let rs = rapidsockets::Client::init(config);
-    rs.connect();
+    let mut rs = rapidsockets::Client::init(config);
 
     fn user_demo(packet: String) {
         println!("{}", packet);
@@ -176,6 +159,8 @@ fn main() {
     subscription.set_channel("user_demo".to_string());
     subscription.set_callback(user_demo);
     subscription.subscribe();
+
+    rs.connect();
 
     let mut message = rapidsockets::Message::init(&rs);
     message.set_channel("user_demo".to_string());
